037880d3cb4b67df36f857da3bf710b4c18a4080
C-DGameMaker <charlotte5634@outlook.com>
2025-10-07 14:38:52 -0300

    Initial commit

---
+++ CharlieDobson_MiniGameWithLoops_Programming1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CharlieDobson_MiniGameWithLoops_Programming1
+{
+    internal class Program
+    {
+        static bool gameOver = false;
+        static int x = 0;
+        static int y = 0;
+        static void Main(string[] args)
+        {
+            while(gameOver == false)
+            {
+
+            }
+        }
+    }
+}
