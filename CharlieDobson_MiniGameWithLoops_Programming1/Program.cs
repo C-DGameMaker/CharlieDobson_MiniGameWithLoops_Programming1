bb35b88211d6c1fd4eca1dcee6ff998bdb2ffc01
C-DGameMaker <charlotte5634@outlook.com>
2025-10-07 15:38:52 -0300

    AddedMainMenu

--- CharlieDobson_MiniGameWithLoops_Programming1/Program.cs
+++ CharlieDobson_MiniGameWithLoops_Programming1/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading;
@@ -14,7 +15,35 @@ namespace CharlieDobson_MiniGameWithLoops_Programming1
         static int y = 0;
         static ConsoleKeyInfo movement = new ConsoleKeyInfo();
 
+        static bool[] visited = { };
+
         static void Main(string[] args)
+        {
+            Console.WriteLine("Makeshift Snake the Game");
+            Console.WriteLine("Press D for Draw mode");
+            Console.WriteLine("Press P for Play mode");
+            Console.WriteLine("Press ESC to quit anytime.");
+
+            ConsoleKeyInfo choice = new ConsoleKeyInfo();
+            choice = Console.ReadKey(true);
+            if(choice.Key == ConsoleKey.D)
+            {
+                Console.Clear();
+                DrawMode();
+            }
+            else if(choice.Key == ConsoleKey.P)
+            {
+                Console.Clear();
+                PlayMode();
+            }
+            
+
+            Console.Clear();
+
+            Console.Write("GAME OVER!");
+        }
+
+        static void DrawMode()
         {
             for (int i = 0; i < 15; i++)
             {
@@ -22,34 +51,65 @@ namespace CharlieDobson_MiniGameWithLoops_Programming1
                 {
                     Console.BackgroundColor = ConsoleColor.Gray;
                     Console.Write(" ");
-                    Thread.Sleep(10);
+                    //Thread.Sleep(10);
                 }
                 Console.Write("\n");
                 Thread.Sleep(10);
             }
-            PlayerDraw(x, y);
+            Console.SetCursorPosition(0, 0);
+            Console.Write("O");
             Console.BackgroundColor = ConsoleColor.Black;
             Console.CursorVisible = false;
 
             while (gameOver == false)
             {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
                 movement = Console.ReadKey(true);
                 Console.SetCursorPosition(x, y);
 
                 PlayerUpdate();
                 PlayerDraw(x, y);
 
+
                 Thread.Sleep(10);
             }
+        }
 
-            Console.Clear();
+        static void PlayMode()
+        {
+            for (int i = 0; i < 15; i++)
+            {
+                for (int j = 0; j < 30; j++)
+                {
+                    Console.BackgroundColor = ConsoleColor.Gray;
+                    Console.Write(" ");
+                    //Thread.Sleep(10);
+                }
+                Console.Write("\n");
+                Thread.Sleep(10);
+            }
+            Console.SetCursorPosition(0, 0);
+            Console.Write("O");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.CursorVisible = false;
 
-            Console.Write("GAME OVER!");
+            while (gameOver == false)
+            {
+                Console.ForegroundColor = ConsoleColor.DarkMagenta;
+                movement = Console.ReadKey(true);
+                Console.SetCursorPosition(x, y);
+
+                PlayerUpdate();
+                PlayerDraw(x, y);
+
+
+                Thread.Sleep(10);
+            }
         }
 
         static void PlayerDraw(int x, int y)
         {
-            Console.BackgroundColor = ConsoleColor.Gray;
+            Console.BackgroundColor = ConsoleColor.Magenta;
             Console.Write(" ");
             Console.ResetColor();
             Console.SetCursorPosition(x, y);
@@ -97,7 +157,11 @@ namespace CharlieDobson_MiniGameWithLoops_Programming1
                 y += 1;
             }
 
+            
+
         }
+
+     
 
        
     }
